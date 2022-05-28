c1e787f4fc126433563a9ce886442d440420ade0
Greyn1 <dheryabengani@outlook.com>
2022-05-29 00:59:13 +0530

    added Shop and ProductCard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { Routes, Route } from 'react-router-dom';
 import Navigation from './Routes/Navigation';
 import Home from './Routes/Home';
+import Shop from './Routes/Shop';
 import Authentication from './Routes/Authentication';
-
-const Shop = () => {
-  return <h1>I am the Shop Page</h1>
-}
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './Contexts/UserContext';
+import { ProductsProvider } from './Contexts/ProductsContext';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ProductsProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </ProductsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
