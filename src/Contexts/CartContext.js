45437cd9bb297f9156d45608d56132d9a5cbf741
Greyn1 <dheryabengani@outlook.com>
2022-05-29 14:36:06 +0530

    added addToCart functionality

--- src/Contexts/CartContext.js
+++ src/Contexts/CartContext.js
@@ -1,13 +1,47 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
+
+const addCartItem = (cartItems, productToAdd) => {
+    //find if productToAdd already exists
+    const existingCartItem = cartItems.find(
+        (cartItem) => cartItem.id === productToAdd.id
+    );
+
+    // if the productToAdd already exists, increase the qty
+    if(existingCartItem){
+        return cartItems.map((cartItem) => 
+            cartItem.id === productToAdd.id
+            ? {...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+        );
+    }
+
+    //if productToAdd doesn't exists, then add it to the cartItems with a qty
+    return [...cartItems, {...productToAdd, quantity:1}];
+};
 
 export const CartContext = createContext({
-    isCartOpen:false,
-    setIsCartOpen: () => {}
+    isCartOpen: false,
+    setIsCartOpen: () => { },
+    cartItems: [],
+    addItemToCart: () => { }, 
+    cartCount :0
 });
 
-export const CartProvider = ({children}) => {
+export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = {isCartOpen, setIsCartOpen};
+    const [cartItems, setCartItems] = useState([]);
+    const [cartCount, setCartCount] = useState(0);
+
+    useEffect(() => {
+        const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity,0);
+        setCartCount(newCartCount);
+    }, [cartItems]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd));
+    }
+
+    const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart, cartCount };
     return (
         <CartContext.Provider value={value} >{children}</CartContext.Provider>
     );
--- src/components/CartDropdown.js
+++ src/components/CartDropdown.js
@@ -1,10 +1,16 @@
+import { useContext } from "react";
+import { CartContext } from "../Contexts/CartContext";
 import Button from "./Button";
+import CartItem from "./CartItem";
 import '../Styles/CartDropdown.styles.scss';
 
 const CartDropdown = () => {
+    const {cartItems} = useContext(CartContext);
     return (
       <div className='cart-dropdown-container'>
-          <div className="cart-items"/>
+          <div className="cart-items">
+              {cartItems.map((item) => <CartItem cartItem={item} key={item.id} />)}
+          </div>
           <Button>CHECKOUT</Button>
       </div>
     );
